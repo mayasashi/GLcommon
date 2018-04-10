6d60edcff7fa36fdaf0e7e43852c5513e02ac90c
鶴拳 <tsuruken@tsurukennoiMac.local>
2018-04-10 16:35:25 +0900

    commit

--- continuousDeformation_project/continuousDeformation_project/main.cpp
+++ continuousDeformation_project/continuousDeformation_project/main.cpp
@@ -15,6 +15,12 @@ int main(int argc, const char * argv[]) {
     if(!glfwInit()){
         printf("failed to init glfw."); return 0;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    
     
     glfwTerminate();
     std::cout << "Hello, World!\n";
