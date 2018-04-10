8817fc7cf3ee04fc08447a9018b2b78fcd5846c7
mayasashi <maruyama_satoshi@mayasashi-no-MacBook-Pro.local>
2018-04-10 21:29:06 +0900

    a new commit from macbook pro

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  continuousDeformation_project
+//
+//  Created by 鶴拳 on 2018/04/04.
+//  Copyright © 2018年 mayasashi. All rights reserved.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
